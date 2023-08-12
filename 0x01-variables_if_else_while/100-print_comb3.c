b069161dd4c3ac387ad5351f968f44bdf5a749ad
adebolagrace6 <adebolagrace6@gmail.com>
2023-08-11 17:54:47 -0700

    A program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main -  Inventing is a combination of brains and materials.
+ * The more brains you use, the less material you need
+ * 
+ * Return: ALways 0 Success
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
