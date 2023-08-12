8f0ace576735db2a65cabb5d3b94fc8ee710fb3d
adebolagrace6 <adebolagrace6@gmail.com>
2023-08-11 17:32:27 -0700

    A program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <string.h>
+/* betty style doc for function main goes there */
+/**
+ * main - When I was having that alphabet soup,
+ * I never thought that it would pay off.
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
