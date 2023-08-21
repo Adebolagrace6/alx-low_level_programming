0183aa5d16599a54c37303d8b9a962b987b8b277
adebolagrace6 <adebolagrace6@gmail.com>
2023-08-21 15:57:45 -0700

    length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,13 @@
 *@s: string
 *Return: length
 */
+
 int _strlen(char *s)
 {
 	int i;
 	int count = 0;
 
-	for (i = 0 ; s[i]; !'\0' : i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
 	return (count);
 
