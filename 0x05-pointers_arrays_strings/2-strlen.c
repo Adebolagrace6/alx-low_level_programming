e4926f2bd05aa2ca823373cd223ff5cd04246b68
adebolagrace6 <adebolagrace6@gmail.com>
2023-08-21 15:26:44 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strlen - return the length of a string
+*@s: string
+*Return: length
+*/
+int _strlen(char *s)
+{
+	int long1 = 0;
+
+	while (*s != '\0')
+	{
+		long i++;
+		s++;
+	}
+	return(long i)
+}
