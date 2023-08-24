1b6ce90def1aa7d03d00dba9929342cd5363fe5e
adebolagrace6 <adebolagrace6@gmail.com>
2023-08-23 17:32:00 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (*(dest + i) != '\0')
+		i++
+
+	while (*(src + j) != '\0' && i < 97 && j < n)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
