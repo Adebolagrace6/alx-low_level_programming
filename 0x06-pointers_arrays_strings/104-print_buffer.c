6da9d5a28390a4db1a3f171aa4285a537ea71041
adebolagrace6 <adebolagrace6@gmail.com>
2023-08-24 15:24:28 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_buffer - prints a buffer.
- * @b: 1st string
- * @size: second string
- * Return: all letters in base ROT13
+ * print_buffer - buffer
+ * @b: char buffer
+ * @size: size
+ * Return: buffer
  */
+
 void print_buffer(char *b, int size)
 {
 	int o, j, i;
@@ -26,22 +28,23 @@ void print_buffer(char *b, int size)
 			if (i < j)
 				printf("%02x", *(b + o + i));
 			else
-				printf(" ");
+				printf("  ");
 			if (i % 2)
+			{
 				printf(" ");
+			}
 		}
-	}
-	for (i = 0; i < j; i++)
-	{
-		int c = *(b + o + i);
-
-		if (c < 32 || c > 132)
+		for (i = 0; i < j; i++)
 		{
-			c = '.';
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
 		}
 		printf("\n");
 		o += 10;
-
 	}
 }
-
