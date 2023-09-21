ae6570a0ccb226caac10d4ab26fe8ba6f1d555f5
Adebolagrace6 <adebolagrace6@gmail.com>
2023-09-21 15:03:13 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - free linked list
+ * @head: pointer to list_t list to be freed
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+list_t *temp;
+
+while (temp = head)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
+}
+
